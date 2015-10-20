3f0b16c99cf1739a76b272465bd25e48433f3d8d
Mike James <m.l.james@me.com>
2015-10-20 12:31:50 +0100

    changed to flexbox for grid, alignment and overflows

--- app/components/Status/Status.jsx
+++ app/components/Status/Status.jsx
@@ -6,15 +6,14 @@ import gravatar from 'gravatar';
 import './_Status.scss';
 
 export default (props) => {
-
-	const imgUrl = gravatar.url(props.email, { s: '400', r: 'pg', d: 'retro'}, true);
+  const imgUrl = gravatar.url(props.email, { s: '400', r: 'pg', d: 'retro'}, true);
 
   return (
     <li className={'item ' + props.status.statusType }>
       <img className='mugshot' src={imgUrl}/>
       <div className='meta'>
-      	<span className="name">{props.name}</span>
-      	<span className="status">{props.status.statusType}</span>
+			<span className='name'>{props.name}</span>
+        <span className='status'>{props.status.statusType}</span>
       </div>
     </li>
   );
