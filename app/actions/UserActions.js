51f90c7d7cba17d14aec89d2e6df13b81bbbf1eb
Mike James <m.l.james@me.com>
2015-11-02 17:41:10 +0000

    group by status

--- app/actions/UserActions.js
+++ app/actions/UserActions.js
@@ -4,14 +4,30 @@ import AppDispatcher from '../dispatcher/AppDispatcher';
 import Constants from '../constants/AppConstants';
 import WebAPI from '../util/WebAPI';
 
+function groupStatus(statuses,statusType) {
+  var s = statuses.filter(worker => {
+    return worker.status.statusType === statusType;
+  });
+
+  return s;
+}
+
 export default {
 
-  getLatestStatuses() {
+  getLatestStatuses(){
 
-    let successHandler = (statuses) => {
+    let successHandler = (statuses)=>{
+
+      let statusesGrouped = [];
+
+      groupStatus(statuses, 'InOffice').forEach(w => statusesGrouped.push(w));
+      groupStatus(statuses, 'OutOfOffice').forEach(w => statusesGrouped.push(w));
+      groupStatus(statuses, 'Holiday').forEach(w => statusesGrouped.push(w));
+      groupStatus(statuses, 'Sick').forEach(w => statusesGrouped.push(w));
+
       AppDispatcher.dispatch({
         type: Constants.LOAD_USERS_SUCCESS,
-        data: statuses
+        data: statusesGrouped
       });
     };
 
