dbd930dd7532aa026f8e714417ea5ba1635fc69e
Mike James <m.l.james@me.com>
2015-11-13 13:45:25 +0000

    Interval of 10 seconds to request updates.

--- app/constants/AppConstants.js
+++ app/constants/AppConstants.js
@@ -3,9 +3,4 @@ import pkg from '../../package';
 export const DEBUG = (process.env.NODE_ENV !== 'production');
 export const APP_TITLE = pkg.name;
 export const NAVIGATION_UPDATED = 'NAVIGATION_UPDATED';
-
-export const LOAD_USERS = 'LOAD_USERS';
-export const LOAD_USERS_SUCCESS = 'LOAD_USERS_SUCCESS';
-export const RECEIVE_USERS = 'RECEIVE_USERS';
-export const LOAD_USERS_FAIL = 'LOAD_USERS_FAIL';
-export const REQUEST_LOAD_USERS = 'REQUEST_LOAD_USERS';
+export const INTERVAL = 10 * 1000;
